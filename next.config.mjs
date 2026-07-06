125253064f995275b84b317da5a53ca0ef6bbd92
sebastian cupajita <latinodeluxe@MacBook-Pro-de-sebastian.local>
2026-07-06 16:15:28 +0200

    Agregar redirect 301 de vercel.app a holapolska.com

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'holapolska.vercel.app' }],
+        destination: 'https://holapolska.com/:path*',
+        permanent: true,
+      },
+    ]
+  },
   async headers() {
     return [
       {
